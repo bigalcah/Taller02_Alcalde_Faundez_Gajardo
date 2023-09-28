9bc1a8c1ca0d0141fccc6921f5a5194cc39134cc
chris <c.gajardo08@ufromail.cl>
2023-09-28 09:47:42 -0300

    Christian Gajardo - commit 1.1 - 9:30

--- src/test/java/MainTest.java
+++ src/test/java/MainTest.java
@@ -10,9 +10,15 @@ import static org.junit.jupiter.api.Assertions.*;
 class MainTest {
     ArrayList<JSONObject> listaHabitaciones;
     ArrayList<JSONObject> listaHabitaciones2;
+    JSONObject Hab;
 
     @BeforeEach
     void setUp() {
+        Hab = new JSONObject();
+        Hab.put("id", 0);
+        Hab.put("estado", "D");
+        Hab.put("noches", 0);
+        Hab.put("precio", 0);
 
     }
 
@@ -30,7 +36,7 @@ class MainTest {
     @Test
     void agregarHabitacion() {
         listaHabitaciones2 = new ArrayList<JSONObject>();
-        Main.agregarHabitacion(listaHabitaciones2);
+        Main.agregarHabitacion(listaHabitaciones2, Hab);
         assertEquals(1, listaHabitaciones2.size());
     }
 }
