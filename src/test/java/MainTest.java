cb99f0df21ffdd625d708edd99d23e99d9fe7a22
jfaundez07 <j.faundez07@ufromail.cl>
2023-09-28 11:22:06 -0300

    Entrega final pruebas

--- src/test/java/MainTest.java
+++ src/test/java/MainTest.java
@@ -9,8 +9,13 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class MainTest {
     ArrayList<JSONObject> listaHabitaciones = new ArrayList<>();
+
+    ArrayList<JSONObject> listaHabitaciones2;
+    JSONObject Hab;
     JSONObject habitacion1 = new JSONObject();
     JSONObject habitacion2 = new JSONObject();
+
+
 
     @BeforeEach
     void setUp() {
@@ -19,6 +24,13 @@ class MainTest {
         habitacion2.put("id",1);
         listaHabitaciones.add(habitacion1);
         listaHabitaciones.add(habitacion2);
+
+        Hab = new JSONObject();
+        Hab.put("id", 0);
+        Hab.put("estado", "D");
+        Hab.put("noches", 0);
+        Hab.put("precio", 0);
+
 
     }
 
@@ -30,4 +42,19 @@ class MainTest {
     void resetAll() {
         assertEquals(0, Main.resetAll(listaHabitaciones)) ;
     }
+
+    @Test
+    void crearHabitaciones() {
+        listaHabitaciones = new ArrayList<JSONObject>();
+        Main.crearHabitaciones(listaHabitaciones);
+        assertEquals(10, listaHabitaciones.size());
+    }
+
+    @Test
+    void agregarHabitacion() {
+        listaHabitaciones2 = new ArrayList<JSONObject>();
+        Main.agregarHabitacion(listaHabitaciones2, Hab);
+        assertEquals(1, listaHabitaciones2.size());
+    }
 }
+
