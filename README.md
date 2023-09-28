64cc22963ff4b496966b16076f2bb10f971ef593
chris <c.gajardo08@ufromail.cl>
2023-09-28 10:11:11 -0300

    Christian Gajardo - commit 2.0 - 10:00

--- README.md
+++ README.md
@@ -6,3 +6,5 @@ Joaquin Faundez
 Actividades realizadas: 
 -metodo para crear 10 habitaciones con cada habitacion como jasonobjet
 -metodo para agregar habitaciones a una array list de jason objets
+
+-metodos encargados de CheckOut
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,6 @@
 import org.json.JSONObject;
 import java.util.ArrayList;
-import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
 
@@ -21,4 +21,25 @@ public class Main {
     public static void agregarHabitacion(ArrayList<JSONObject> listaHabitaciones, JSONObject json) {
         listaHabitaciones.add(json);
     }
+
+    public static int encontrarHabitacion(ArrayList<JSONObject> listaHabitaciones, String ID){
+        int index = 0;
+        for (int indice = 0; indice < listaHabitaciones.size(); indice ++){
+            if ((listaHabitaciones.get(indice)).get("id") == ID){
+                 index = indice;
+            }
+        }
+        return index;
+    }
+    public static void checkout(JSONObject Habitacion){
+        Habitacion.put("estado", "D");
+        Habitacion.put("noches", 0);
+        Habitacion.put("precio", 0);
+    }
+
+    public static  void imprimirCCheckout(JSONObject Habitacion){
+
+    }
+
+
 }
