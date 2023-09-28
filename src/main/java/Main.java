0a8dc1bbd363ffac9686a5116f9f009c454b7617
jfaundez07 <j.faundez07@ufromail.cl>
2023-09-28 09:44:17 -0300

    Avance 1. Metodo RessetAll y prueba ResetAll

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,50 @@
+import org.json.JSONObject;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+/*
+
+{
+"id" = 0,
+"estado" = (D,R,OA,OS) ,
+"noches" = 0,
+"precio" = 0
+}
+
+*/
 public class Main {
+
     public static void main(String[] args) {
 
     }
+
+    public static String lectura(){
+        Scanner leer = new Scanner(System.in);
+        return leer.next();
+    }
+
+    public static void B_resetAll(ArrayList<JSONObject> listaHabitaciones) {
+
+        System.out.println("Esta seguro que desea resetear todas las habiaciones?" +
+                "Esto eliminara todas las reservas y/o cabañas ocupadas.");
+
+        String seleccion = lectura();
+
+        if (seleccion.equals("0")) {
+
+            resetAll(listaHabitaciones);
+
+        } else {
+            System.out.println("No se resetearon las habitaciones.");
+        }
+
+    }
+
+    public static int resetAll(ArrayList<JSONObject> listaHabitaciones) {
+        listaHabitaciones.clear();
+        return listaHabitaciones.size();
+    }
+
+
+
 }
